ad143b2b7cd5d940d57944cc66f49e3e9b6b42cb
dago19 <alevanni19@gmail.com>
2015-05-27 19:13:24 +0200

    Il client manda messaggi al server

--- client.py
+++ client.py
@@ -5,8 +5,18 @@ import sys
 class client:
   def __init__(self, host):
     #costruisco il sockt del client
+    self.esci=False
     self.sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    self.sock.connect((host, 4000))
-
+    self.sock.connect((host, 5003))
+    #data=self.sock.recv(1024)
+    #print data
+    while not self.esci:
+      data=raw_input("Manda al server:")
+      if data != 'quit':
+        self.sock.sendall(data)
+      else :
+        self.esci=True
+        self.sock.sendall('connessione interrotta')
+    self.sock.close()
 
 prova=client("alessia-Aspire-5536")
--- server.py
+++ server.py
@@ -7,21 +7,31 @@ class server:
   def __init__(self, porta):
     # crea un socket INET di tipo STREAM
     self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.uscito=False
     # associa il socket a un host pubblico
     self.serversocket.bind((socket.gethostname(), porta))
     print porta, socket.gethostname()
     # diventa un socket server
     self.serversocket.listen(5)
+    # accetta le connessioni dall'esterno
+    (socketclient, address) = self.serversocket.accept()
+    
+    # ora fa qualcosa con il socket client
+    print socketclient, address
+    #socketclient.send('Write something')
     while 1:
-      # accetta le connessioni dall'esterno
-      (socketclient, address) = self.serversocket.accept()
-      # ora fa qualcosa con il socket client
-      print socketclient, address
-    #while  
+      data= socketclient.recv(1024)
+      if not data:
+        break
+      else:
+        print data
+       
+        
+        
     self.serversocket.close()
-  #avviaserver
+      
 #server
 
 #avvio il server sulla porta 4000
-prova=server(4000)
+prova=server(5003)
 
